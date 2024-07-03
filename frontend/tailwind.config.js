bd7e709896fd766d62d72ec35c7e894a6a26f357
Thuyen Code <thuyencode@proton.me>
2024-07-03 10:26:10 +0700

    Use Inter as the default font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 import daisyui from 'daisyui'
+import * as defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [daisyui]
 }
