3a8e1ddde93b5608eb7bb6698ab3d97c4ab4f719
Thuyen Code <thuyencode@proton.me>
2024-07-03 10:23:31 +0700

    The Navbar and Footer are not completed yet

--- frontend/src/layout/Footer.tsx
+++ frontend/src/layout/Footer.tsx
@@ -1,7 +1,11 @@
 import { type ReactElement } from 'react'
 
 function Footer(): ReactElement {
-  return <footer className='footer'></footer>
+  return (
+    <footer className='footer footer-center bg-base-200 p-4'>
+      <aside>Thuyen Code</aside>
+    </footer>
+  )
 }
 
 export default Footer
--- frontend/src/layout/Navbar.tsx
+++ frontend/src/layout/Navbar.tsx
@@ -1,7 +1,11 @@
 import { type ReactElement } from 'react'
 
 function Navbar(): ReactElement {
-  return <nav className='navbar'></nav>
+  return (
+    <nav className='navbar shadow-xl'>
+      <div className='navbar-start'>Navbar</div>
+    </nav>
+  )
 }
 
 export default Navbar
