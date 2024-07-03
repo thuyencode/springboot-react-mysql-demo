476f5ccfcf6ed563e26a929b4e625e180600ed40
Thuyen Code <thuyencode@proton.me>
2024-07-03 19:52:13 +0700

    Understandable error messages

--- frontend/src/libs/schema.ts
+++ frontend/src/libs/schema.ts
@@ -1,22 +1,29 @@
 import * as v from 'valibot'
 
 const REGEX_WHITESPACE_ERROR = 'Must not contains whitespace(s)'
+const MIN_LENGTH_ERROR = 'The minium length must longer than 3 characters'
+const MAX_LENGTH_ERROR = 'The maximum length must not exceeds 255 characters'
 
 export const UserFormSchema = v.object({
-  name: v.pipe(v.string(), v.trim(), v.minLength(3), v.maxLength(255)),
+  name: v.pipe(
+    v.string(),
+    v.trim(),
+    v.minLength(3, MIN_LENGTH_ERROR),
+    v.maxLength(255, MAX_LENGTH_ERROR)
+  ),
   username: v.pipe(
     v.string(),
     v.trim(),
     v.regex(/[^\s-]/, REGEX_WHITESPACE_ERROR),
-    v.minLength(3),
-    v.maxLength(255)
+    v.minLength(3, MIN_LENGTH_ERROR),
+    v.maxLength(255, MAX_LENGTH_ERROR)
   ),
   email: v.pipe(
     v.string(),
     v.trim(),
     v.regex(/[^\s-]/, REGEX_WHITESPACE_ERROR),
     v.email(),
-    v.minLength(3),
-    v.maxLength(255)
+    v.minLength(3, MIN_LENGTH_ERROR),
+    v.maxLength(255, MAX_LENGTH_ERROR)
   )
 })
