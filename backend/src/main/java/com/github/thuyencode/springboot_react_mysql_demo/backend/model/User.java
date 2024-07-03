37f71b4b0f116080dfd959f8988377cb9208f663
Thuyen Code <thuyencode@proton.me>
2024-07-03 16:19:09 +0700

    Specify the limitation of given values

--- backend/src/main/java/com/github/thuyencode/springboot_react_mysql_demo/backend/model/User.java
+++ backend/src/main/java/com/github/thuyencode/springboot_react_mysql_demo/backend/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,10 +21,13 @@ public class User {
   private Long id;
 
   @Column(unique = true)
+  @Size(min = 3, max = 255)
   private String username;
 
+  @Size(min = 3, max = 255)
   private String name;
 
   @Column(unique = true)
+  @Size(min = 3, max = 255)
   private String email;
 }
