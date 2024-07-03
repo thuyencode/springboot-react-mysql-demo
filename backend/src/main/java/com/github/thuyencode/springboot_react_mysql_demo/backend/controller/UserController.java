a9b50426545c391f1e10ca10fc51fc91b95e3b32
Thuyen Code <thuyencode@proton.me>
2024-07-03 11:22:00 +0700

    Enable CORS

--- backend/src/main/java/com/github/thuyencode/springboot_react_mysql_demo/backend/controller/UserController.java
+++ backend/src/main/java/com/github/thuyencode/springboot_react_mysql_demo/backend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.github.thuyencode.springboot_react_mysql_demo.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import com.github.thuyencode.springboot_react_mysql_demo.backend.model.User;
 import com.github.thuyencode.springboot_react_mysql_demo.backend.repository.UserRepository;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class UserController {
 
   @Autowired
