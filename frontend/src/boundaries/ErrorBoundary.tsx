3cda54c7ea88b5e7ce5f209d5ff70e2a04dc6c34
Thuyen Code <thuyencode@proton.me>
2024-07-03 10:55:05 +0700

    Implement the error boundary

---
+++ frontend/src/boundaries/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+// Thanks to https://stackoverflow.com/a/78132377
+
+import { Component, type ErrorInfo, type ReactNode } from 'react'
+
+interface ErrorBoundaryProps {
+  children: ReactNode
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error('Error caught by ErrorBoundary:', error, errorInfo)
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return <h2 className='text-error'>Something went wrong.</h2>
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
