a60cd74db030ea7f97cf17f5292c3da84bdd04c7
Thuyen Code <thuyencode@proton.me>
2024-07-03 09:29:41 +0700

    Create Model and Repo for database mapping

---
+++ backend/src/main/java/com/github/thuyencode/springboot_react_mysql_demo/backend/model/User.java
@@ -0,0 +1,22 @@
+package com.github.thuyencode.springboot_react_mysql_demo.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String username;
+  private String name;
+  private String email;
+}
