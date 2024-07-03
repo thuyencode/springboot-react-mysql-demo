4fe345a3a386c33e65eca4b0ec0cc2b852397200
Thuyen Code <thuyencode@proton.me>
2024-07-03 22:26:40 +0700

    Use indexOf instead of regex

--- frontend/src/libs/schema.ts
+++ frontend/src/libs/schema.ts
@@ -1,6 +1,6 @@
 import * as v from 'valibot'
 
-const REGEX_WHITESPACE_ERROR = 'Must not contains whitespace(s)'
+const WHITESPACE_ERROR = 'Must not contains whitespace(s)'
 const MIN_LENGTH_ERROR = 'The minium length must longer than 3 characters'
 const MAX_LENGTH_ERROR = 'The maximum length must not exceeds 255 characters'
 
@@ -16,14 +16,14 @@ export const UserFormSchema = v.object({
     v.trim(),
     v.minLength(3, MIN_LENGTH_ERROR),
     v.maxLength(255, MAX_LENGTH_ERROR),
-    v.regex(/[^\s-]/, REGEX_WHITESPACE_ERROR)
+    v.check((str) => str.indexOf(' ') === -1, WHITESPACE_ERROR)
   ),
   email: v.pipe(
     v.string(),
     v.trim(),
     v.minLength(3, MIN_LENGTH_ERROR),
     v.maxLength(255, MAX_LENGTH_ERROR),
-    v.email(),
-    v.regex(/[^\s-]/, REGEX_WHITESPACE_ERROR)
+    v.check((str) => str.indexOf(' ') === -1, WHITESPACE_ERROR),
+    v.email()
   )
 })
