30b51378cc49066179b60b46e733c4d794fd18ca
Thuyen Code <thuyencode@proton.me>
2024-07-03 08:32:52 +0700

    Spring Boot as the backend

---
+++ backend/src/test/java/com/github/thuyencode/springboot_react_mysql_demo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thuyencode.springboot_react_mysql_demo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
